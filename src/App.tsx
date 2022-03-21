85087f34e7172329cf470b976104d762e6ad37b5
TutusBOT <barte77pl@gmail.com>
2022-03-21 02:13:05 +0100

    allows user to sign up,in and out

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,33 @@ import {
 	getDocs,
 	DocumentData,
 } from "firebase/firestore";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+	createUserWithEmailAndPassword,
+	getAuth,
+	onAuthStateChanged,
+	User,
+	signOut,
+	signInWithEmailAndPassword,
+	updateProfile,
+} from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import firebaseConfig from "./components/firebaseConfig";
+import SignUp from "./components/SignUp";
+import CreatePost from "./components/CreatePost";
+
+const firebaseApp = initializeApp(firebaseConfig);
+let auth = getAuth(firebaseApp);
+
+console.log("test", auth);
 
 function App() {
 	const [searchInput, setSearchInput] = useState("");
 	const [posts, setPosts] = useState<DocumentData>();
+	const [logged, setLogged] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [openCreatePost, setOpenCreatePost] = useState(false);
 	// 	[
 	// 	{
 	// 		username: "TutusBOT",
@@ -38,25 +58,106 @@ function App() {
 			return reqPostsData;
 		};
 		test();
+
+		// const checkauth = getAuth(firebaseApp);
+		// const a = checkauth;
+		// console.log("start", a.currentUser);
 	}, []);
-	const user = authentication("", "");
+	// useEffect(() => {
+	// 	console.log("jd");
+	// }, [auth.currentUser]);
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setLogged(true);
+			} else {
+				setLogged(false);
+			}
+
+			console.log("user", user);
+		});
+	}, []);
 	return (
 		<div className="App">
-			<Header searchInput={searchInput} setSearchInput={setSearchInput} />
-			<div className="posts">
-				{posts
-					? posts.map((post: IPosts) => {
-							return (
-								<Post
-									key={post.id}
-									username={post.username}
-									caption={post.caption}
-									imageUrl={post.imageUrl}
-								/>
-							);
-					  })
-					: ""}
-			</div>
+			<Header
+				searchInput={searchInput}
+				setSearchInput={setSearchInput}
+				open={openCreatePost}
+				setOpen={setOpenCreatePost}
+			/>
+			{openCreatePost ? <CreatePost /> : null}
+			{auth.currentUser ? (
+				<>
+					<button
+						onClick={() => {
+							signOut(auth);
+							auth = getAuth(firebaseApp);
+						}}
+					>
+						Sign out
+					</button>
+					<div className="posts">
+						{posts
+							? posts.map((post: IPosts) => {
+									return (
+										<Post
+											key={post.id}
+											username={post.username}
+											caption={post.caption}
+											imageUrl={post.imageUrl}
+										/>
+									);
+							  })
+							: ""}
+					</div>
+				</>
+			) : (
+				<form
+					className="authentication"
+					onSubmit={(e) => {
+						e.preventDefault();
+					}}
+				>
+					username:{" "}
+					<input
+						type="text"
+						value={username}
+						onChange={(e) => {
+							setUsername(e.target.value);
+						}}
+					/>
+					email:{" "}
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
+					/>{" "}
+					password:{" "}
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
+					/>
+					<button
+						onClick={() => {
+							signUp(email, password, username);
+						}}
+					>
+						Sign Up
+					</button>{" "}
+					<button
+						onClick={() => {
+							signIn(email, password);
+						}}
+					>
+						Sign in
+					</button>
+				</form>
+			)}
 		</div>
 	);
 }
@@ -66,7 +167,7 @@ interface IPosts {
 	caption: string;
 	imageUrl: string;
 }
-const firebaseApp = initializeApp(firebaseConfig);
+
 async function FB() {
 	const db = getFirestore();
 	const colRef = collection(db, "posts");
@@ -99,11 +200,29 @@ async function FB() {
 	const storage = getStorage(firebaseApp);
 	return data;
 }
-function authentication(email: string, password: string) {
-	const auth = getAuth(firebaseApp);
+function signUp(email: string, password: string, username: string) {
+	if (!email || !password) return;
+	console.log(auth);
+
 	createUserWithEmailAndPassword(auth, email, password)
 		.then((userCredential) => {
 			const user = userCredential.user;
+			updateProfile(userCredential.user, {
+				displayName: username,
+			});
+			console.log("usercredential", user);
+			return user;
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+}
+function signIn(email: string, password: string) {
+	if (!email || !password) return;
+	signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			const user = userCredential.user;
+			auth = getAuth(firebaseApp);
 			return user;
 		})
 		.catch((e) => {
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,10 @@
+function SignUp() {
+	return (
+		<>
+			email: <input type="email" /> password: <input type="password" />
+			<button>Sign Up</button> <button>Sign in</button>
+		</>
+	);
+}
+
+export default SignUp;
