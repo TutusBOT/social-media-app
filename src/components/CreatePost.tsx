c6f4fa43f1cb0444f9124a594c5b3f1156d6cf2f
TutusBOT <barte77pl@gmail.com>
2022-03-25 23:03:39 +0100

    minor changes

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,7 +1,112 @@
-function CreatePost() {
+import { Dispatch, SetStateAction, useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {
+	collection,
+	addDoc,
+	getFirestore,
+	Timestamp,
+	serverTimestamp,
+} from "firebase/firestore";
+import { storage } from "../App";
+import { User } from "firebase/auth";
+
+function CreatePost({
+	user,
+	show,
+	handleShow,
+}: {
+	user: null | User;
+	show: boolean;
+	handleShow: Dispatch<SetStateAction<boolean>>;
+}) {
+	const [image, setImage] = useState<any>();
+	const [caption, setCaption] = useState("");
+	const [progressPCT, setProgressPCT] = useState(0);
+	const db = getFirestore();
+	const handleChange = (event: any) => {
+		if (event.target.files[0]) {
+			setImage(event.target.files[0]);
+			console.log(event.target.files[0]);
+		}
+		console.log("chuj", user);
+	};
+
+	const handleUpload = () => {
+		const imageRef = ref(storage, `images/${image.name}`);
+		const uploadImage = uploadBytesResumable(imageRef, image);
+		uploadImage.on(
+			"state_changed",
+			(snapshot) => {
+				// Observe state change events such as progress, pause, and resume
+				// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				console.log("Upload is " + progress + "% done");
+				setProgressPCT(progress);
+				switch (snapshot.state) {
+					case "paused":
+						console.log("Upload is paused");
+						break;
+					case "running":
+						console.log("Upload is running");
+						break;
+				}
+			},
+			(error) => {
+				// Handle unsuccessful uploads
+				console.log("error with uploading", error);
+			},
+			() => {
+				// Handle successful uploads on complete
+				// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+				getDownloadURL(uploadImage.snapshot.ref).then((downloadURL) => {
+					const docRef = addDoc(collection(db, "posts"), {
+						username: user?.displayName,
+						caption: caption,
+						imageUrl: downloadURL,
+						timestamp: serverTimestamp(),
+					});
+					//   console.log('File available at', downloadURL);
+				});
+			}
+		);
+	};
 	return (
-		<div className="createpost">
-			<input type="text" />
+		<div
+			className="createpost"
+			style={{ display: show ? "flex" : "none", opacity: show ? "1" : "0" }}
+			onClick={() => {
+				handleShow(false);
+			}}
+		>
+			<div
+				className="createpost-wrapper"
+				onClick={(e) => {
+					e.stopPropagation();
+				}}
+			>
+				<h2>Create new post</h2>
+				<input
+					type="text"
+					value={caption}
+					onChange={(e) => {
+						setCaption(e.target.value);
+					}}
+				/>
+				<input
+					type="file"
+					onChange={(e) => {
+						handleChange(e);
+					}}
+				/>
+				<button
+					onClick={() => {
+						handleUpload();
+					}}
+				>
+					Post
+				</button>
+			</div>
 		</div>
 	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { User } from "firebase/auth";
 import SearchBar from "./SearchBar";
 import UserMenu from "./UserMenu";
 
@@ -6,17 +7,30 @@ function Header({
 	setSearchInput,
 	open,
 	setOpen,
+	user,
+	logOutButton,
 }: {
 	searchInput: string;
 	setSearchInput: any;
 	open: boolean;
 	setOpen: any;
+	user: null | User;
+	logOutButton: JSX.Element;
 }) {
 	return (
 		<nav className="navbar">
 			<div className="navbar-logo">LOGO</div>
 			<SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
-			<UserMenu open={open} setOpen={setOpen} />
+			{user ? (
+				<UserMenu
+					open={open}
+					setOpen={setOpen}
+					profilePicture={user.photoURL}
+					logOut={logOutButton}
+				/>
+			) : (
+				<></>
+			)}
 		</nav>
 	);
 }
